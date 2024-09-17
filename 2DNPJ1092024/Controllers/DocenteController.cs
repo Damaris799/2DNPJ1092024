53df5577c0c1b2b616108e056ab8db47c04850d7
Damaris5416 <damarisnayeliperezjuarez01@gmail.com>
2024-09-16 13:47:17 -1200

    Agregar archivos de proyecto.

---
+++ 2DNPJ1092024/Controllers/DocenteController.cs
@@ -0,0 +1,73 @@
+﻿using _2DNPJ1092024.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace _2DNPJ1092024.Controllers
+
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class DocentesController : ControllerBase
+    {
+        static List<Docente> docentes = new List<Docente>();
+
+        [HttpGet]
+
+        public IEnumerable<Docente> Get()
+        {
+            return docentes;
+        }
+
+        [HttpGet("{id}")]
+
+        public Docente Get(int id)
+        {
+            var docente = docentes.FirstOrDefault(c => c.Id == id);
+            return docente;
+        }
+
+        [HttpPost]
+
+        public IActionResult Post([FromBody] Docente docente)
+        {
+            docentes.Add(docente);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Docente docente)
+        {
+            var existingDocente = docentes.FirstOrDefault(c => c.Id == id);
+            if (existingDocente != null)
+            {
+                existingDocente.Nombre = docente.Nombre;
+                existingDocente.Apellido = docente.Apellido;
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        // DELETE api/<ClientsController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var  existingDocente = docentes.FirstOrDefault(c => c.Id == id);
+            if (existingDocente != null)
+            {
+                docentes.Remove(existingDocente);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+    }
+}
+
---
+++ 2DNPJ1092024/Models/Docente.cs
@@ -0,0 +1,11 @@
+﻿namespace _2DNPJ1092024.Models
+{
+    public class Docente
+    {
+        public int Id { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+    }
+}
